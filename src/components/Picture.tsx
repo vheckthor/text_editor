eaaacb686367ccac7964492ce7fba17f97ab3316
TunarshBee <rafiubello4real@gmail.com>
2023-07-13 10:13:37 +0100

    Done with all the media file

---
+++ src/components/Picture.tsx
@@ -0,0 +1,78 @@
+import React, { useContext, useRef } from "react";
+import ReactModal from "react-modal";
+import { FaTimes } from "react-icons/fa";
+import { EditorContext } from "../context/EditorContext";
+
+interface ModalProps {
+  onFileChange: (file: File) => void;
+  isOpen: boolean;
+  isClose: () => void;
+}
+
+const Picture: React.FC<ModalProps> = ({
+  onFileChange,
+  isOpen,
+  isClose,
+}) => {
+  const { incrementWordCount } = useContext(EditorContext);
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleFileChange = () => {
+    const file = ref.current?.files?.[0];
+    if (file) {
+      onFileChange(file);
+      isClose();
+      incrementWordCount();
+    }
+  };
+
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={isClose}
+      contentLabel="Modal"
+      className="custom-modal bg-[#a1a1aa]"
+    >
+      <div className="bg-white rounded-md p-3">
+        <div className="flex items-center justify-between mb-4 px-2">
+          <p className="font-bold px-1 text-[16px]">Embed</p>
+          <FaTimes className="cursor-pointer mr-2" onClick={isClose} />
+        </div>
+        <div className="px-3">
+          <p className="mb-2 text-[14px]">Upload Image</p>
+          <small className="text-[10px] uppercase">File upload</small>
+          <div className="bg-[#FAFAFA] border border-dashed border-green-700 flex h-[141px] w-[611px] items-center justify-center mt-2 mb-3 mx-auto">
+            <button
+              onClick={() => {
+                ref.current?.click();
+              }}
+              className="bg-white border-solid border border-[#6CAA7D] p-2 px-4 rounded-[4px] text-[10px]"
+            >
+              Import Image from Device
+              <input
+                type="file"
+                id="fileInput"
+                style={{ display: "none" }}
+                onChange={handleFileChange}
+                ref={ref}
+              />
+            </button>
+          </div>
+          <div className="py-2 text-[14px]">
+            <button className="bg-[#0A7227] hover:bg-green-700 mr-2 text-white rounded-md">
+              Embed
+            </button>
+            <button
+              className="border-solid border border-[#CEE3D4] mr-2 rounded-md"
+              onClick={isClose}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </ReactModal>
+  );
+};
+
+export default Picture;
---
+++ src/components/SocialMedia.tsx
@@ -0,0 +1,129 @@
+import React, { useState, useContext } from "react";
+import ReactModal from "react-modal";
+import { FaTimes } from "react-icons/fa";
+import { EditorContext } from "../context/EditorContext";
+
+interface ModalProps {
+  isOpen: boolean;
+  isClose: () => void;
+  LinkorVideo: (content: string, type: "link" | "video") => void;
+}
+
+const SocialMedia: React.FC<ModalProps> = ({
+  isOpen,
+  isClose,
+  LinkorVideo,
+}) => {
+  const { incrementWordCount } = useContext(EditorContext);
+  const socials = ["Facebook", "Twitter", "Instagram", "TikTok"];
+  const [toggleCaption, setToggleCaption] = useState(false);
+  const [provider, setProvider] = useState("");
+  const [code, setCode] = useState("");
+  const [url, setUrl] = useState("");
+
+  const handleProviderInput = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setProvider(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log(provider);
+    console.log(code);
+    console.log(url);
+    const linkContent = `<a href="${url}" target="_blank">${code}</a>`;
+    LinkorVideo(linkContent, "link");
+    incrementWordCount()
+    isClose();
+  };
+
+
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={isClose}
+      contentLabel="Modal"
+      className="custom-modal bg-[#a1a1aa]"
+    >
+      <div className="bg-white rounded-md p-3">
+        <div className="flex items-center justify-between mb-4 px-2">
+          <p className="font-bold px-1">Embed</p>
+          <FaTimes className="cursor-pointer mr-2" onClick={isClose} />
+        </div>
+        <div className="px-3">
+          <div className="flex flex-col h-[56px] my-4 w-[611px]">
+            <label className="mb-2 social-media text-[10px] uppercase">
+              Social Media Platform
+            </label>
+            <select
+              onChange={handleProviderInput}
+              className="bg-[#FAFAFA] border border-gray-200 h-[34px] outline-gray-300 px-2 text-[12px]"
+              name="social-media"
+              id=""
+            >
+              
+              {socials.map((obj, idx) => (
+                <option key={idx} value={obj}>
+                  {obj}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="flex flex-col h-[56px] my-4 w-[611px]">
+            <label className="mb-2 social-media text-[10px] uppercase">
+              url
+            </label>
+            <input
+              onChange={(e) => setUrl(e.target.value)}
+              className="bg-[#FAFAFA] border border-gray-200 h-[34px] outline-gray-300 px-2 text-[12px]"
+              name="url"
+              placeholder=""
+              id=""
+            />
+          </div>
+
+          <div className="flex flex-col h-[56px] my-4 w-[611px]">
+            <label className="mb-2 social-media text-[10px] uppercase">
+              Code
+            </label>
+            <input
+              onChange={(e) => setCode(e.target.value)}
+              className="bg-[#FAFAFA] border border-gray-200 h-[34px] outline-gray-300 px-2 text-[12px]"
+              name="code"
+              placeholder=""
+              id=""
+            />
+          </div>
+
+          <div className="mb-2 flex items-center justify-between">
+            <p className="text-[12px]">Disable caption</p>
+            <div
+              className={
+                toggleCaption ? "toggle-container" : "toggle-container on"
+              }
+              onClick={() => setToggleCaption(!toggleCaption)}
+            >
+              <div className="toggle-handle"></div>
+            </div>
+          </div>
+
+          <div className="py-2 text-[14px]">
+            <button
+              className="bg-green-800 hover:bg-green-700 mr-2 text-white rounded-md"
+              onClick={handleSubmit}
+            >
+              Embed
+            </button>
+            <button
+              className="border-solid border border-[#CEE3D4] mr-2 rounded-md"
+              onClick={isClose}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </ReactModal>
+  );
+};
+
+export default SocialMedia;
