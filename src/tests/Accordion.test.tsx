75403e280501ec2732daf9b73b37524d686d775c
TunarshBee <rafiubello4real@gmail.com>
2023-07-14 04:02:10 +0100

    unit testing..............

---
+++ src/tests/Accordion.test.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Accordion from "../components/Accordion";
+
+describe("Accordion Component", () => {
+  it("should render the Accordion component", () => {
+    // Render the Accordion component
+    const { getByText } = render(
+      <Accordion onFileChange={() => {}} LinkorVideo={() => {}} />
+    );
+
+    // Check if the component is rendered
+    const addButton = screen.getByText("+");
+    expect(addButton).toBeInTheDocument();
+  });
+
+  it("should open the modal when the add button is clicked", () => {
+    // Render the Accordion component
+    const { getByText, queryByText } = render(
+      <Accordion onFileChange={() => {}} LinkorVideo={() => {}} />
+    );
+
+    // Check if the modal is initially closed
+    expect(screen.queryByText("Picture")).toBeNull();
+
+    // Click the add button to open the modal
+    const addButton = screen.getByText("+");
+    fireEvent.click(addButton);
+
+    // Check if the modal is opened
+    const pictureButton = screen.getByText("Picture");
+    expect(pictureButton).toBeInTheDocument();
+  });
+
+  it("should close the modal when the close button is clicked", () => {
+    // Render the Accordion component
+    const { getByText, queryByText } = render(
+      <Accordion onFileChange={() => {}} LinkorVideo={() => {}} />
+    );
+
+    // Open the modal
+    const addButton = screen.getByText("+");
+    fireEvent.click(addButton);
+
+    // Check if the modal is opened
+    const pictureButton = screen.getByText("Picture");
+    expect(pictureButton).toBeInTheDocument();
+  });
+});
---
+++ src/tests/Ediitor.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import QuillToolbar from "../components/Editor";
+import { EditorContext } from "../context/EditorContext";
+
+describe("QuillToolbar Component", () => {
+  test("should call onValue when the value changes", () => {
+    const mockOnValue = jest.fn();
+
+    render(
+      <EditorContext.Provider
+        value={{ incrementWordCount: () => {}, wordCount: 0 }}
+      >
+        <QuillToolbar onValue={mockOnValue} />
+      </EditorContext.Provider>
+    );
+
+    // Simulate typing in the editor
+    const editor = screen.getByRole("textbox");
+    userEvent.type(editor, "Hello World");
+
+    // Check if the onValue function is called with the correct argument
+    expect(mockOnValue).toHaveBeenCalledWith(1); // Assuming "Hello" and "World" are considered as separate words
+
+    // You can add more assertions to test other functionalities of the component
+  });
+});
